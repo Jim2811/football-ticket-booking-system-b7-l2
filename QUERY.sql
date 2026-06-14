0a7bbfb57a00847b5f6f0d045987a12189c4ca55
Rifat <mdrifat107590@gmail.com>
2026-06-15 02:54:59 +0600

    created matches table

--- QUERY.sql
+++ QUERY.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS Bookings;
 DROP TABLE IF EXISTS Matches;
 DROP TABLE IF EXISTS Users;
 
-
+-- users table
 CREATE TABLE Users (
     user_id       SERIAL,
     full_name     VARCHAR(100)        NOT NULL,
@@ -15,7 +15,17 @@ CREATE TABLE Users (
     CONSTRAINT chk_users_role CHECK (role IN ('Ticket Manager', 'Football Fan'))
 );
 
-
+--matches table
+CREATE TABLE Matches (
+    match_id SERIAL,
+    fixture VARCHAR(200) NOT NULL,
+    tournament_category VARCHAR(100) NOT NULL,
+    base_ticket_price NUMERIC(10, 2) NOT NULL,
+    match_status VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_matches PRIMARY KEY (match_id),
+    CONSTRAINT chk_matches_price CHECK (base_ticket_price >= 0),
+    CONSTRAINT chk_matches_status CHECK (match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
+);
 
 
 
